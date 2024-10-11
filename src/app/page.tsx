12c762791cd71e5ed86b8ad0a211db2f4204104c
Euan Morgan <euanmorgan48@gmail.com>
2024-10-11 08:55:35 +0100

    feat: customise components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      Hello world
-      <Button variant="destructive" size="lg">
-        Click me
+    <div className="flex gap-4 p-4 flex-wrap">
+      <Input />
+      <Button variant="primary" size="lg">
+        Primary
       </Button>
-      <p className="text-red-500">Hello</p>
+      <Button variant="secondary" size="lg">
+        Secondary
+      </Button>
+      <Button variant="destructive" size="lg">
+        Destructive
+      </Button>
+      <Button variant="ghost" size="lg">
+        Ghost
+      </Button>
+      <Button variant="muted" size="lg">
+        Muted
+      </Button>
+      <Button variant="outline" size="lg">
+        Outline
+      </Button>
+      <Button variant="tertiary" size="lg">
+        Tertiary
+      </Button>
     </div>
   );
 }
